a66803601dd1c6b1f83df3096c33298b96697a63
Fluffydragon02 <148916400+Fluffydragon02@users.noreply.github.com>
2023-12-07 15:04:39 -0800

    12/07

--- Game_Project_Group29/Assets/Scripts/Weapon.cs
+++ Game_Project_Group29/Assets/Scripts/Weapon.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public GameObject player;
+    public Vector3 weaponPos;
     public Vector3 offset;
     
 
     // Start is called before the first frame update
     void Start()
     {
+        //weaponPos = player.GetComponent<PlayerController1>().transform.position;
         offset = transform.position - player.transform.position;
     }
 
@@ -18,5 +20,12 @@ public class Weapon : MonoBehaviour
     void Update()
     {
         transform.position = player.transform.position + offset;
+        /*weaponPos = player.GetComponent<PlayerController1>().transform.position;
+        if (weaponPos.y <= 1.3f)
+        {
+            weaponPos.y += 0.35f;
+
+        }
+        transform.position = weaponPos;*/
     }
 }
